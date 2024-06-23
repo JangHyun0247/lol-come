86e2acbfcf8b22b321dd366d5a8dc6582247529d
JangHyun0247 <yugi82828@naver.com>
2024-06-23 13:05:12 +0900

    init commit

---
+++ src/main/java/com/sparta/lolcome/LolComeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.lolcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolComeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolComeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/lolcome/LolComeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.lolcome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolComeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
